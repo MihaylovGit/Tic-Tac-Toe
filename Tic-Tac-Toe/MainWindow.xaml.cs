eb4ce4c4b27987716a7f892b458383321940ea7a
Kostadin Mihaylov <kmihaylov@90gmail.com>
2023-02-23 11:03:44 +0200

    Add UI in xaml file and some logic for rendering player movement on the grid

--- Tic-Tac-Toe/MainWindow.xaml.cs
+++ Tic-Tac-Toe/MainWindow.xaml.cs
@@ -15,14 +15,68 @@ using System.Windows.Shapes;
 
 namespace Tic_Tac_Toe
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Dictionary<Player, ImageSource> imageSources = new()
+        {
+            { Player.X, new BitmapImage(new Uri("pack://application:,,,/Assets/X15.png")) },
+            { Player.O, new BitmapImage(new Uri("pack://application:,,,/Assets/O15.png")) },
+        };
+
+        private readonly Image[,] imageControls = new Image[3, 3];
+        private readonly GameState gameState = new GameState();
+
         public MainWindow()
         {
             InitializeComponent();
+            SetupGameGrid();
+
+            gameState.MoveMade += OnMoveMade;
+            gameState.GameEnded += OnGameEnded;
+            gameState.GameRestarted += OnGameRestarted; 
+        }
+
+        private void SetupGameGrid()
+        {
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Image imageControl = new Image();
+                    GameGrid.Children.Add(imageControl);
+                    imageControls[r, c] = imageControl;
+                }
+            }
+        }
+
+        private void OnMoveMade(int r, int c)
+        {
+
+        }
+
+        private void OnGameEnded(GameResult gameResult)
+        {
+
+        }
+
+        private void OnGameRestarted(GameResult gameResult)
+        {
+
+        }
+
+        private void GameGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            double squareSize = GameGrid.Width / 3;
+            Point clickPosition = e.GetPosition(GameGrid);
+            int row = (int)(clickPosition.Y / squareSize);
+            int col = (int)(clickPosition.X / squareSize);
+
+            gameState.MakeMove(row, col);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
